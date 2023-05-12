9ccf4821f8e72df213796d4ab192b4c71baba2e0
putrija <91309853+putrija@users.noreply.github.com>
2023-05-12 21:39:49 +0700

    ubah ke butstrap 5.0.2

--- resources/views/pengajuan/form.blade.php
+++ resources/views/pengajuan/form.blade.php
@@ -7,8 +7,8 @@
     <title>Form Upload Berkas</title>
     <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet'>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
     <!-- load Dropzone.js library -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.9.2/min/dropzone.min.js"></script>
     <!-- load Dropzone.js CSS -->
