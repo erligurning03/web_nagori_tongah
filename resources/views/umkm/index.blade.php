348120951c2de7eb58c7541c3b6e4c3e9541cc81
AgnesTS <agnestryanisim@gmail.com>
2023-05-20 17:10:18 +0700

    umkm dan register

---
+++ resources/views/umkm/index.blade.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+  <title>UMKM Desa</title>
+  <style>
+    .card {
+      margin-bottom: 20px;
+    }
+    .background{
+        background-color: gray;
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-4 col-md-6">
+        <div class="card">
+          <img src="img/umkm1.png" class="card-img-top" alt="Gambar 1">
+          <div class="card-body">
+                <h5 class="card-title"><i class="fa-solid fa-shop"></i><b>  Fresh Mart</b></h5>
+            <p class="card-text">Alamat: Jalan Arteri Raya No.17</p>
+            <p class="card-text">Telepon: 0812-2671-9243 (Antonio Saragih)</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6">
+        <div class="card">
+          <img src="img/umkm2.png" class="card-img-top" alt="Gambar 2">
+          <div class="card-body">
+            <h5 class="card-title"><i class="fa-solid fa-utensils"></i><b>  Sate  Madura</b></h5>
+            <p class="card-text">Alamat: Jalan Gondang Jati No. 88</p>
+            <p class="card-text">Telepon: 0856-2162-0977 (Imelda Purba)</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6">
+        <div class="card">
+          <img src="img/umkm3.png" class="card-img-top" alt="Gambar 3">
+          <div class="card-body">
+            <h5 class="card-title"><i class="fa-solid fa-cake-candles"></i><b>  Amanda</b></h5>
+            <p class="card-text">Alamat: Jalan Mawar No. 03</p>
+            <p class="card-text">Telepon: 0822-2120-0003 (Amanda Naibaho)</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-4 col-md-6">
+        <div class="card">
+          <img src="img/umkm4.png" class="card-img-top" alt="Gambar 4">
+          <div class="card-body">
+            <h5 class="card-title"><i class="fa-solid fa-shop"></i><b> Mitra Tani</b></h5>
+            <p class="card-text">Lokasi: Jalan Rajamin Purba No. 21</p>
+            <p class="card-text">Telepon: 0821-7628-0971 (Hasian Damanik)</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6">
+        <div class="card">
+          <img src="img/umkm5.png" class="card-img-top" alt="Gambar 5">
+          <div class="card-body">
+            <h5 class="card-title"><i class="fa-solid fa-shop"></i><b>  Toko Sinar Jaya</b></h5>
+              <p class="card-text">Alamat: Jalan Rajawali No. 63</p>
+              <p class="card-text">Telepon: 0813-1267-8125 (Marito Sinaga)</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6">
+        <div class="card">
+          <img src="img/umkm6.png" class="card-img-top" alt="Gambar 5">
+          <div class="card-body">
+            <h5 class="card-title"><i class="fa-solid fa-shop"></i><b>  Subur Pupuk</b></h5>
+              <p class="card-text">Alamat: Jalan Rajawali No. 3</p>
+              <p class="card-text">Telepon: 0813-7373-9271 (Anggara Manik)</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
