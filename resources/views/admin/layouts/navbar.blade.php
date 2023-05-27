c9c59306bc200e23b43938db9ef42447858b3ceb
PutriAnisa123 <pa374167@gmail.com>
2023-05-27 08:36:43 +0700

    Perbaikan Navbar admin - Putri anisa
    Signed-off-by: PutriAnisa123 <pa374167@gmail.com>

--- resources/views/admin/layouts/navbar.blade.php
+++ resources/views/admin/layouts/navbar.blade.php
@@ -72,23 +72,6 @@
                     </div>
                 </div>
             </li>
-
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseOneTwo"
-                    aria-expanded="true" aria-controls="collapseOneTwo">
-                    <i class="fa fa-map" aria-hidden="true"></i>
-                    <span>Wisata
-                    </span>
-                </a>
-                <div id="collapseOneTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Pilih Aksi:</h6>
-                        <a class="collapse-item" href="{{ url('/admin/wisatadesa') }}">Daftar Wisata Desa</a>
-                        <a class="collapse-item" href="{{ url('/admin/tambah/perangkat') }}">Tambah Wisata</a>
-                    </div>
-                </div>
-            </li>
-
             <!-- Nav Item - Utilities Collapse Menu -->
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
