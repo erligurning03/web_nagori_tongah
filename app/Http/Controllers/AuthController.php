069fcd8791a57f8cdb7098e7be7dbee5fa9f4e68
putrija <91309853+putrija@users.noreply.github.com>
2023-06-05 03:05:30 +0700

    Putrija - perbaiki sedikit auth controller
    membuat agar logout ke landing
    membuat agar setiap register maka auth:logout terlebih dahulu

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -23,7 +23,7 @@ class AuthController extends Controller
     public function register()
     {
         // return view('auth.register');
-        // Auth::logout();
+        Auth::logout();
         $isAuthenticated = Auth::check();
 
         return view('auth.register', compact('isAuthenticated'));
@@ -144,7 +144,7 @@ class AuthController extends Controller
         // Perform the logout
         Auth::logout();
 
-        return redirect()->route('login')->withHeaders([
+        return redirect()->route('landing')->withHeaders([
             'Cache-Control' => 'no-store, no-cache, must-revalidate, max-age=0',
             'Pragma' => 'no-cache',
             'Expires' => 'Sat, 01 Jan 2000 00:00:00 GMT',
