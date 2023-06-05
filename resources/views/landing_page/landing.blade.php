9d02a923b8f27a113ee894576d474a12849cfc5a
erligurning03 <erligurning03@gmail.com>
2023-06-05 07:05:33 +0700

    erli -- animasi landing
    smoth scrol dan animasi.

--- resources/views/landing_page/landing.blade.php
+++ resources/views/landing_page/landing.blade.php
@@ -3,57 +3,67 @@
 
 {{-- Utama --}}
 <section id="utama">
-	<header>
-<div class="header-content">
-	<h1>NAGORI NAGORITONGAH</h1>
-	<div class="line"></div>
-	<h2>Nagori Nagoritongah adalah sebuah desa di Sumatera Utara dengan keindahan alam dan masyarakat yang ramah. <br> Terdapat berbagai destinasi wisata seperti air terjun, hutan pinus, dan kebun teh yang menarik untuk dikunjungi.</h2>
-</div>
-	</header>
+	<div class="box" data-aos="fade-up" data-aos-duration="1000">
+		<header>
+		<div class="header-content">
+			<h1>NAGORI NAGORITONGAH</h1>
+			<div class="line"></div>
+			<h2>Nagori Nagoritongah adalah sebuah desa di Sumatera Utara dengan keindahan alam dan masyarakat yang ramah. <br> Terdapat berbagai destinasi wisata seperti air terjun, hutan pinus, dan kebun teh yang menarik untuk dikunjungi.</h2>
+		</div>
+		</header>
+	</div>
 </section>
 
 {{-- Fitur --}}
 <section id="fitur">
-<div class="row d-flex flex-wrap">
-	<div class="card-group">
-	  <div class="col-sm-2 mx-auto">
-		  <div class="card">
-			<div class="card-body text-center">
-			  <a href="login"><img src="img/4.png" class="card-img-top-1" alt="..."></a>
-			  <h5 class="card-title"><b>pengurusan berkas</b></h5>
-			  <p class="card-text">Keperluan pengurusan berkas seperti KTP, KK, dan lain sebagainya.</p>
+	<div class="row d-flex flex-wrap">
+		<div class="card-group">
+		<div class="col-sm-2 mx-auto">
+			<div class="box">
+			<div class="card" data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="100">
+				<div class="card-body text-center">
+				<a href="login"><img src="img/4.png" class="card-img-top-1" alt="..."></a>
+				<h5 class="card-title"><b>pengurusan berkas</b></h5>
+				<p class="card-text">Keperluan pengurusan berkas seperti KTP, KK, dan lain sebagainya.</p>
+				</div>
 			</div>
-		  </div>
-	  </div>
-	  <div class="col-sm-2 mx-auto">
-		  <div class="card">
-			<div class="card-body text-center">
-			  <a href="login"><img src="img/1.png" class="card-img-top-1" alt="..."></a>
-			  <h5 class="card-title"><b>Forum Diskusi</b></h5>
-			  <p class="card-text">Untuk berbagi informasi, ide, pendapat, dan tempat pengaduan</p>
 			</div>
-		  </div>
-	  </div>
-	  <div class="col-sm-2 mx-auto">
-		  <div class="card">
-			<div class="card-body text-center">
-			  <a href="umkm"><img src="img/3.png" class="card-img-top-1" alt="..."></a> 
-			  <h5 class="card-title"><b>UMKM Nagori</b></h5>
-			  <p class="card-text">Kumpulan UMKM yang terdapat di Nagori, daftarkan UKMK Anda!</p>
+		</div>
+		<div class="col-sm-2 mx-auto">
+			<div class="box" data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="200">
+			<div class="card" >
+				<div class="card-body text-center">
+				<a href="login"><img src="img/1.png" class="card-img-top-1" alt="..."></a>
+				<h5 class="card-title"><b>Forum Diskusi</b></h5>
+				<p class="card-text">Untuk berbagi informasi, ide, pendapat, dan tempat pengaduan</p>
+				</div>
 			</div>
-		  </div>
-	  </div>
-	  <div class="col-sm-2 mx-auto">
-		  <div class="card">
-			<div class="card-body text-center">
-				<a href="#"><img src="img/4.png" class="card-img-top-1" alt="..."></a>
-			  <h5 class="card-title"><b>Transparansi Dana</b></h5>
-			  <p class="card-text">Informasi pendapatan dan pengeluaran Dana pertahun dan realisasinya.</p>
 			</div>
-		  </div>
-	  </div>
-  </div>
-</div>
+		</div>
+		<div class="col-sm-2 mx-auto">
+			<div class="box" data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="300">
+			<div class="card">
+				<div class="card-body text-center">
+				<a href="umkm"><img src="img/3.png" class="card-img-top-1" alt="..."></a> 
+				<h5 class="card-title"><b>UMKM Nagori</b></h5>
+				<p class="card-text">Kumpulan UMKM yang terdapat di Nagori, daftarkan UKMK Anda!</p>
+				</div>
+			</div>
+			</div>
+		</div>
+		<div class="col-sm-2 mx-auto">
+			<div class="box" data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="400">
+			<div class="card">
+				<div class="card-body text-center">
+					<a href="#"><img src="img/4.png" class="card-img-top-1" alt="..."></a>
+				<h5 class="card-title"><b>Transparansi Dana</b></h5>
+				<p class="card-text">Informasi pendapatan dan pengeluaran Dana pertahun dan realisasinya.</p>
+				</div>
+			</div>
+			</div>
+		</div>
+	</div>
+	</div>
 </section>
 
 {{-- Visi Misi --}}
@@ -68,26 +78,32 @@
 		  <div class="col-md-4 mb-3 text-center">
 			<img src="../img/component_4.png" class="card-img-top-2" alt="...">
 		  </div>
-		  <div class="col-md-4 text-center">
+		  <div class="col-md-4 text-center" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="200">
+			<div class="box">
 			  <h2>VISI</h2>
 			  <p>Meningkatkan kesejahteraan masyarakat yang bermartabat dan religius dengan mengembangkan potensi sumber daya. </p>
+			</div>
 		  </div>
 		  <div class="col-md-4 text-center">
+			<div class="box" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="300">
 			  <h2>MISI</h2>
 			  <P>Memberikan pelayanan yang positif kepada masyarakat dan membuat rencana pembangunan diberbagai sektor baik pertanian maupun perekonomian</P>
+			</div>
 		  </div>
 		</div>
 	  </div>
 </section>
 
 {{-- Perangkat Desa --}}
-<section>
+<section id="perangkat">
 	 <div class="container2">
             <div class="row">
               <h1><b>STRUKTUR PERANGKAT DESA</b></h1>
               <div class="col-sm-3 mx-auto">
                 <div class="card text-center">
-                  <img src="../img/gideon.jpg" class="card-img-top-3" alt="...">
+					<div class="box" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="100">
+                  		<img src="../img/gideon.jpg" class="card-img-top-3" alt="...">
+					</div>
                   <div class="card-body">
                     <h5 class="card-title">Kepala Desa</h5>
                     <p class="card-text">Gideon Manurung</p>
@@ -97,61 +113,73 @@
             </div>
             <div class="row">
               <div class="col-sm-3 mx-auto">
-                <div class="card text-center">
-                  <img src="../img/perangkat1.png" class="card-img-top-3" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">Wakil Kepala Desa</h5>
-                    <p class="card-text">Putrija Malau</p>
-                  </div>
-                </div>
+				<div class="box" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="100">
+					<div class="card text-center">
+					<img src="../img/perangkat1.png" class="card-img-top-3" alt="...">
+					<div class="card-body">
+						<h5 class="card-title">Wakil Kepala Desa</h5>
+						<p class="card-text">Putrija Malau</p>
+					</div>
+					</div>
+				</div>
               </div>
               <div class="col-sm-3 mx-auto">
-                <div class="card text-center">
-                  <img src="../img/Perangkat1.png" class="card-img-top-3" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">Sekretaris Desa</h5>
-                    <p class="card-text">Putri Anisa</p>
-                  </div>
-                </div>
+				<div class="box" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="200">
+					<div class="card text-center">
+					<img src="../img/Perangkat1.png" class="card-img-top-3" alt="...">
+					<div class="card-body">
+						<h5 class="card-title">Sekretaris Desa</h5>
+						<p class="card-text">Putri Anisa</p>
+					</div>
+					</div>
+				</div>
               </div>
               <div class="col-sm-3 mx-auto">
-                <div class="card text-center">
-                  <img src="../img/Perangkat1.png" class="card-img-top-3" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">Wakil Sekretaris Desa</h5>
-                    <p class="card-text">Agnes Tryani</p>
-                  </div>
-                </div>
+				<div class="box" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="300">
+					<div class="card text-center">
+					<img src="../img/Perangkat1.png" class="card-img-top-3" alt="...">
+					<div class="card-body">
+						<h5 class="card-title">Wakil Sekretaris Desa</h5>
+						<p class="card-text">Agnes Tryani</p>
+					</div>
+					</div>
+				</div>
               </div>
 			  {{-- line baru --}}
 			  <br>
 			  <div class="row">
 				<div class="col-sm-3 mx-auto">
-				  <div class="card text-center">
-					<img src="../img/Perangkat1.png" class="card-img-top-3" alt="...">
-					<div class="card-body">
-					  <h5 class="card-title">Bendahara Desa</h5>
-					  <p class="card-text">Gabryelle Ninna</p>
+					<div class="box" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="100">
+						<div class="card text-center">
+							<img src="../img/Perangkat1.png" class="card-img-top-3" alt="...">
+							<div class="card-body">
+							<h5 class="card-title">Bendahara Desa</h5>
+							<p class="card-text">Gabryelle Ninna</p>
+							</div>
+						</div>
 					</div>
-				  </div>
 				</div>
 				<div class="col-sm-3 mx-auto">
-				  <div class="card text-center">
-					<img src="../img/Perangkat1.png" class="card-img-top-3" alt="...">
-					<div class="card-body">
-					  <h5 class="card-title">Wakil Bendahara Desa</h5>
-					  <p class="card-text">Erli Gurning</p>
+					<div class="box" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="200">
+						<div class="card text-center">
+							<img src="../img/Perangkat1.png" class="card-img-top-3" alt="...">
+							<div class="card-body">
+							<h5 class="card-title">Wakil Bendahara Desa</h5>
+							<p class="card-text">Erli Gurning</p>
+							</div>
+						</div>
 					</div>
-				  </div>
 				</div>
 				<div class="col-sm-3 mx-auto">
-				  <div class="card text-center">
-					<img src="../img/Perangkat1.png" class="card-img-top-3" alt="...">
-					<div class="card-body">
-					  <h5 class="card-title">Kepala Desa</h5>
-					  <p class="card-text">Gideon Manurung.</p>
+					<div class="box" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="300">
+						<div class="card text-center">
+							<img src="../img/Perangkat1.png" class="card-img-top-3" alt="...">
+							<div class="card-body">
+							<h5 class="card-title">Kepala Desa</h5>
+							<p class="card-text">Gideon Manurung.</p>
+							</div>
+						</div>
 					</div>
-				  </div>
 				</div>
             </div>
           </div>
@@ -159,38 +187,44 @@
 
 <hr>
 {{-- berita --}}
-<section>
+<section id="berita">
 	<div class="container3">
 		<div class="row margin-buttom:20px">
 		  <h1><b>BERITA</b></h1>
-		  <div class="col-sm-3 mx-auto">
-			<div class="card-1">
-				<img src="../img/1.jpg" class="card-img-top" alt="...">
-				<div class="card-body">
-				  <h5 class="card-title-1">Petunjuk Teknis Pengurusan Surat Keterangan Untuk Dibawa Ke Kantor Camat</h5>
-				  <p class="card-text-1">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-				  <p class="card-text-2">18 Mei 2023</p>
+		  	<div class="col-sm-3 mx-auto">
+				<div class="box" data-aos="zoom-in-right" data-aos-duration="1000" data-aos-delay="100">
+					<div class="card-1">
+						<img src="../img/1.jpg" class="card-img-top" alt="...">
+						<div class="card-body">
+							<h5 class="card-title-1">Petunjuk Teknis Pengurusan Surat Keterangan Untuk Dibawa Ke Kantor Camat</h5>
+							<p class="card-text-1">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							<p class="card-text-2">18 Mei 2023</p>
+						</div>
+					</div>
 				</div>
-			  </div>
+			</div>
+		  <div class="col-sm-3 mx-auto">
+			<div class="box" data-aos="zoom-in-right" data-aos-duration="1000" data-aos-delay="100">
+			  	<div class="card-1">
+					<img src="../img/1.jpg" class="card-img-top" alt="...">
+					<div class="card-body">
+					<h5 class="card-title-1">Desa Nagori - Nagoritongah Mengadakan Ibadah Bersama Anak - Anak Setempat</h5>
+					<p class="card-text-1">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<p class="card-text-2">24 Oktober 2023</p>
+					</div>
+			 	</div>
+			</div>
 		  </div>
 		  <div class="col-sm-3 mx-auto">
-			  <div class="card-1">
+			<div class="box" data-aos="zoom-in-right" data-aos-duration="1000" data-aos-delay="100">
+				<div class="card-1">
 				<img src="../img/1.jpg" class="card-img-top" alt="...">
 				<div class="card-body">
-				  <h5 class="card-title-1">Desa Nagori - Nagoritongah Mengadakan Ibadah Bersama Anak - Anak Setempat</h5>
-				  <p class="card-text-1">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-				  <p class="card-text-2">24 Oktober 2023</p>
+					<h5 class="card-title-1">Pemilihan Pangulu Nagori Di 248 Nagori Nagoritongah Kabupaten Simalungun </h5>
+					<p class="card-text-1">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<p class="card-text-2">30 Oktober 2023</p>
 				</div>
-			  </div>
-		  </div>
-		  <div class="col-sm-3 mx-auto">
-			<div class="card-1">
-			  <img src="../img/1.jpg" class="card-img-top" alt="...">
-			  <div class="card-body">
-				<h5 class="card-title-1">Pemilihan Pangulu Nagori Di 248 Nagori Nagoritongah Kabupaten Simalungun </h5>
-				<p class="card-text-1">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-				<p class="card-text-2">30 Oktober 2023</p>
-			  </div>
+				</div>
 			</div>
 		</div>
 		<div class="d-grid gap-2 d-md-flex justify-content-md-center">
