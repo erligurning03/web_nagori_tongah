9472c239a6f54adf275a93e84587d8beb7cfc58e
putrija <91309853+putrija@users.noreply.github.com>
2023-06-06 13:41:04 +0700

    Putrija - memperbaiki sedikit ketikan salah di suket.blade.php

--- resources/views/admin/pengajuan/suket.blade.php
+++ resources/views/admin/pengajuan/suket.blade.php
@@ -105,10 +105,4 @@
     <a class="scroll-to-top rounded" href="#page-top">
         <i class="fas fa-angle-up"></i>
     </a>
-
-<<<<<<< Updated upstream
-
-
-=======
->>>>>>> Stashed changes
 @endsection
