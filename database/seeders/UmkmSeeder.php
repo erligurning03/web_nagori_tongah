2ea2d4a8da1cb83474c7a3f8a60bc3cc0eef8d23
AgnesTS <agnestryanisim@gmail.com>
2023-06-08 01:05:07 +0700

    agnes
    seeder umkm

--- database/seeders/UmkmSeeder.php
+++ database/seeders/UmkmSeeder.php
@@ -15,22 +15,45 @@ class UmkmSeeder extends Seeder
     {
         DB::table('umkm')->insert([
             'id' => 1,
-            'nama_usaha' => 'Mitra Tani',
-            'alamat' => 'Jl. Mawar No. 123',
-            'gambar_produk' => 'gambar1.jpg',
+            'nik' => '1234567890432156',
+            'upload_ktp' => 'ktp1.jpg',
+            'pas_foto' => 'pas1.jpg',
+            'nama_usaha' => 'Bolu Amanda ',
+            'alamat' => 'Jl. Mutiara',
             'telepon' => '08123456789',
+            'gambar_produk' => 'umkm3.png',
             'deskripsi' => 'Toko ini menyediakan berbagai macam kebutuhan pokok sehari-hari seperti beras, gula, minyak goreng, tepung, telur, sayuran, dan buah-buahan segar.',
+            'status_validasi' => 'menunggu',
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s'),
         ]);
 
         DB::table('umkm')->insert([
-            'id' => 1,
+            'id' => 2,
+            'nik' => '1111111122222222',
+            'upload_ktp' => 'ktp2.jpg',
+            'pas_foto' => 'pas2.jpg',
             'nama_usaha' => 'Rumah Makan Halak Kita',
-            'alamat' => 'Jl. Arteri Raya N0. 17',
-            'gambar_produk' => 'gambar2.jpg',
+            'alamat' => 'Jl. Harmoni',
             'telepon' => '08234567890',
+            'gambar_produk' => 'gambar2.jpg',
             'deskripsi' => 'Toko ini menyediakan berbagai macam kebutuhan pokok sehari-hari seperti beras, gula, minyak goreng, tepung, telur, sayuran, dan buah-buahan segar.',
+            'status_validasi' => 'menunggu',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('umkm')->insert([
+            'id' => 3,
+            'nik' => '1839890822775882',
+            'upload_ktp' => 'ktp4.png',
+            'pas_foto' => 'pas3.jpeg',
+            'nama_usaha' => 'Toko Sinar Jaya',
+            'alamat' => 'Jl. Harmoni No.17',
+            'telepon' => '08234567213',
+            'gambar_produk' => 'umkm5.png',
+            'deskripsi' => 'Toko ini menyediakan berbagai macam kebutuhan pokok sehari-hari seperti beras, gula, minyak goreng, tepung, telur, sayuran, dan buah-buahan segar.',
+            'status_validasi' => 'menunggu',
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s'),
         ]);
--- resources/views/admin/berita/semuaberita.blade.php
+++ resources/views/admin/berita/semuaberita.blade.php
@@ -127,7 +127,7 @@
         <i class="fas fa-angle-up"></i>
     </a>
 
-    <!-- Logout Modal-->
+    {{-- <!-- Logout Modal-->
     <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
         aria-hidden="true">
         <div class="modal-dialog" role="document">
@@ -145,6 +145,6 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
 
 @endsection
